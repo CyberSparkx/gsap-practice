0cfea84b4f43f182c4d32680324ce96d854c8970
CyberSparkx <narensarkar607@gmail.com>
2026-06-21 13:15:30 +0530

    font color and all changed

--- SGP web library/src/components/Hero.jsx
+++ SGP web library/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="w-full h-screen items-center justify-center flex flex-col">
         <div>
-            <div className="w-full h-32  flex items-center justify-between absolute top-0 left-0 px-6">
+            <div className="w-full h-32  flex items-center justify-between absolute top-0 left-0 px-6 text-semibold ">
             <p className="text-lg text-center">SGP Web Library</p>
             <p className="text-center text-lg">Created by</p>
             <p className="text-lg text-center">Naren Roy</p>
@@ -13,8 +13,8 @@ const Hero = () => {
         </div>
 
         <div>
-            <h1 className="text-5xl font-bold text-center">Welcome to SGP Web Library</h1>
-            <p className="text-center mt-4 text-lg">A small part of memories in the form of code.</p>
+            <h1 className="text-5xl font-bold text-center text-[#633ea3]">Welcome to SGP Web Library</h1>
+            <p className="text-center mt-4 text-2xl text-semibold">A small part of memories in the form of code.</p>
         </div>
 
     </div>
