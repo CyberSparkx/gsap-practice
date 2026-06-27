b9a0d6a7edba07ba75de24955bed9640a6977215
CyberSparkx <narensarkar607@gmail.com>
2026-06-27 23:54:34 +0530

    scroll based text split animation done

---
+++ gsap-p10/script.js
@@ -0,0 +1,29 @@
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const split = new SplitText(".text", { type: "lines, words, chars" });
+
+const scrollTween = gsap.to(".text", {
+  xPercent: -100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".wrap",
+    pin: true,
+    end: "+=4000px",
+    scrub: true,
+  }
+});
+
+split.chars.forEach((char) => {
+  gsap.from(char, {
+    yPercent: "random(-200, 200)",
+    rotation: "random(-20, 20)",
+    ease: "back.out(1.2)",
+    scrollTrigger: {
+      trigger: char,
+      containerAnimation: scrollTween,
+      start: "left 100%",
+      end: "left 30%",
+      scrub: 1
+    }
+  });
+});
