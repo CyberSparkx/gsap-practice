20a463ed00bd96879dd30e0c3949fcaa965e647f
CyberSparkx <narensarkar607@gmail.com>
2026-06-28 13:23:38 +0530

    Tailwind, gsap & project setup done

---
+++ p1/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Gsap Project 1</h1>
+    </div>
+  )
+}
+
+export default App
